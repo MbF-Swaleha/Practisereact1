79d49533934056c571380098d4c6e43949c51b13
Swaleha Tariq <swaleha@madebyfire.com>
2018-09-19 11:58:58 +0100

    Changes to CRUD adding delete

--- crud-app-manual/src/App.js
+++ crud-app-manual/src/App.js
@@ -1,78 +1,90 @@
 import React, { Component } from 'react';
 import './App.css';
+import NameList from'./NameList.js';
 
-class App extends Component {
-	constructor(props) {
-		super(props);
-		this.deleteTask =this.deleteTask.bind(this);
-		this.state = {
-		  data:
-		  [
-             {
-             	"id":1,
-                "name": "Simon"
+const names = [
+  {
+              "id":1,
+                "name": "Sandy"
              },
              {
-             	"id":2,
+              "id":2,
                 "name": "Stuart"
              },
              {
-             	"id":3,
+              "id":3,
                 "name": "Ben"
              },
              {
-             	"id":4,
+              "id":4,
                 "name": "Kerri"
              },
              {
-             	"id":5,
+              "id":5,
                 "name": "Laiqah"
              }
-		  ]
-		}
 
-	}
-    deleteTask(names){
-    	var index= this.state.names.indexOf(names);
-    	           this.state.names.splice(index,1);
-    	           this.setState(this.state.names);
+];
 
-    };
-  render() {
+localStorage.setItem('names', JSON.stringify(names));
+
+
+class App extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        names: JSON.parse(localStorage.getItem('names'))
+      };
+
+      this.onDelete = this.onDelete.bind(this);
+    }
+
+   componentWillMount() {   
+    const names = this.getNames();
+
+    this.setState({ names });
+   }
+   getNames()
+{
+  return this.state.names;
+    
+   }
+
+   onDelete (name){
+       console.log(name);
+      const names = this.getNames();
+
+      const filteredNames = names.filter(name => {
+        return name.name !== name;
+      });
+
+         console.log(filteredNames)
+      this.setState({names: filteredNames})
+   }
+	render() {
     return (
-
       <div className = "table">
-      <h1>List of names</h1>
-        <table>
-          <tbody>
-            <tr>
-            <td>ID</td>
-            <td>Name</td>
-
-            </tr>
-            {this.state.data.map((person, i) => <TableRow key = {i} data = {person} />  )}
-           </tbody>
-         </table>
+       <h1> Name List </h1>
+       <table>
+       <tr><td>ID</td>
+       {` | `}
+       <td>Name</td></tr>
+      </table>
+       {
+        this.state.names.map(name => {
+         return (
+           <NameList
+             key={name.name}
+             {...name}
+            onDelete={this.onDelete}
+           />
+         );
+       })
+     }
       </div>
     );
   }
 }
-
-  class TableRow extends Component {
-    render() {
-    	return (
-             <tr>             
-               <td>{this.props.data.id}</td>
-               <td>{this.props.data.name}</td>
-               <td><input type="button" value="Delete" className="delete" /></td> 
-
-               
-             </tr>
-      );
-    }
-
-  }
-
-
 
 export default App;
